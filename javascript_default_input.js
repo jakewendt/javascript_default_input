07e4a8f971360413b75f0bb29d43e1937345e181
Jake Wendt <github@jake.otherinbox.com>
2009-01-06 23:46:11 -0700

    Got plain javascript version working

--- javascript_default_input.js
+++ javascript_default_input.js
@@ -8,4 +8,38 @@
 			class: password
 				Converts the field to a "password" once the user has entered one.
 				Converts the field back to a "text" if empty.
+
 */
+
+
+function find_default_inputs() {
+	inputs = document.getElementsByTagName('input');
+	for( i=0; i<=inputs.length; i++){
+		if( inputs[i] && inputs[i].className && inputs[i].className.match('default') ) {
+			inputs[i].onfocus = default_input_focus
+			inputs[i].onblur  = default_input_blur
+			inputs[i].value = inputs[i].id
+			inputs[i].type  = 'text'
+		}
+	}
+}
+
+function default_input_blur() {
+	if ( this.value == "" ) {
+		this.value = this.id;
+		if ( this.className.match( 'password' ) ) {
+			this.type = 'text';
+		}
+	}
+}
+
+function default_input_focus() {
+	if ( this.value == this.id ) {
+		this.value = "";
+		if ( this.className.match( 'password' ) ) {
+			this.type = 'password';
+		}
+	}
+}
+
+window.onload = find_default_inputs;
